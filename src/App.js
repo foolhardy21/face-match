0301db588e40a09134841bb28df44eae51c9f3b1
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2022-12-09 13:39:52 +0530

    [feat]: face matching

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Webcam from "react-webcam";
+import { loadSsdMobilenetv1Model, loadFaceLandmarkModel, loadFaceRecognitionModel, detectAllFaces, bufferToImage, loadFaceExpressionModel, detectSingleFace, LabeledFaceDescriptors, FaceMatcher, resizeResults } from 'face-api.js'
 
 const videoConstraints = {
   width: 1280,
@@ -7,11 +8,23 @@ const videoConstraints = {
   facingMode: "user"
 };
 
+const MODEL_URL = '/models'
+
 function App() {
   const [img1, setImg1] = useState('')
   const [img2, setImg2] = useState('')
-
   const webcamRef = useRef(null);
+
+  useEffect(() => {
+    (async () => {
+      await loadSsdMobilenetv1Model(MODEL_URL)
+      await loadFaceLandmarkModel(MODEL_URL)
+      await loadFaceRecognitionModel(MODEL_URL)
+      await loadFaceExpressionModel(MODEL_URL)
+    })()
+  }, [])
+
+
   const capture =
     () => {
       const imageSrc = webcamRef.current.getScreenshot();
@@ -22,8 +35,22 @@ function App() {
       }
     };
 
-  function compare() {
+  async function compare() {
+    const img = document.getElementById('img1')
+    const faceDescription = await detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+    const faceDescriptors = [faceDescription.descriptor]
+    const labeledFaceDescriptors = new LabeledFaceDescriptors('first image', faceDescriptors)
+    console.log(labeledFaceDescriptors)
 
+    const faceMatcher = new FaceMatcher(labeledFaceDescriptors, .6)
+    console.log(faceMatcher)
+
+    const img2 = document.getElementById('img2')
+    let faceDescription2 = await detectSingleFace(img2).withFaceLandmarks().withFaceDescriptor().withFaceExpressions()
+    faceDescription2 = resizeResults(faceDescription2, img2)
+
+    const results = faceMatcher.findBestMatch(faceDescription2.descriptor)
+    console.log(results)
   }
 
   return (
@@ -60,11 +87,11 @@ function App() {
           flexDirection: 'column',
           marginLeft: '20px',
         }}>
-          <img src={img1} alt='' style={{
+          <img id='img1' src={img1} alt='' style={{
             width: '500px',
             height: 'auto'
           }} />
-          <img src={img2} alt='' style={{
+          <img id='img2' src={img2} alt='' style={{
             width: '500px',
             height: 'auto'
           }} />
